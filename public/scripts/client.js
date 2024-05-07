d9a014ae48c70ac62a97b24142cc4d33241a8656
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-05-06 22:36:46 -0600

    Add dynamic tweets and create client.js file to hold tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,51 +4,70 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function () {
-	const newTweet = [
-		{
-			user: {
-				name: 'Newton',
-				avatars: 'https://i.imgur.com/73hZDYK.png',
-				handle: '@SirIsaac',
-			},
-			content: {
-				text: 'If I have seen further it is by standing on the shoulders of giants',
-			},
-			created_at: 1461116232227,
-		},
-	];
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  
+];
 
-	const createTweetElement = (newTweet) => {
-		const { name, avatars, handle } = newTweet[0].user;
-		const message = newTweet[0].content.text;
-		const createdAt = newTweet[0].created_at;
 
-		const tweetHTML = `
-			<article>
-				<div class="tweet-header">
-					<div class="profile">
-						<img src="${avatars}" alt="" id="avatars"/>
-						<span id="sender-name">${name}</span>
-					</div>
-					<div id="tweeter-account">${handle}</div>
-				</div>
-				<p class="tweet-text">${message}</p>
-				<div class="more-info">
-					<div id="created-at">${createdAt}</div>
-					<div class="icons">
-						<i class="fas fa-flag fa-xs"></i>
-						<i class="far fa-retweet fa-xs"></i>
-						<i class="far fa-heart fa-xs"></i>
-					</div>
-				</div>
-			</article>
-		`;
+$(() => {
+  const createTweetElement = (tweet) => {
+    //const newTweet = $(`<article class="tweet"> ${tweet.content.text} </article>`);
+    const newTweet = $(`
+        <article class="tweet"> 
+        <header>
+          <div class="profile">
+            <img src="${tweet.user.avatars}"> 
+            <span>${tweet.user.name}</span>
+          </div>
+          <div class="user_id">
+            <span>${tweet.user.handle}</span>
+          </div>
+        </header>
+        <p>
+          ${tweet.content.text}
+        </p>
+        <footer>
+          <p>${tweet.created_at}</p>
+          <div>
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-repeat"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `)
+    return newTweet;
+  };
 
-		return tweetHTML;
-	};
 
-	const $tweet = createTweetElement(newTweet);
-	$('main').append($tweet);
-	// console.log($tweet);
+  const renderTweets = (data) => {
+    for (let tweet in data) {
+      $('#tweet-container').append(createTweetElement(data[tweet]));
+    }
 
+  };
+
+  renderTweets(data);
+
+});
